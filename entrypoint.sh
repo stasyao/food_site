65df0e7617a4691945516a6ce5834534029a3906
stas <garanzha.s.a@gmail.com>
2021-03-28 04:45:03 +0300

    add commands to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,7 @@ python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py loaddata fixtures.json
+python manage.py thumbnail cleanup
+python manage.py thumbnail clear
 
 gunicorn config.wsgi -b 0.0.0.0:8000
